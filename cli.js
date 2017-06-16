ab0f2d0e7efc52ec8ff6ae529d0107cf7956b853
Daniel Walker <polyesterhat@gmail.com>
2017-06-15 21:18:25 -0500

    add a copy key command

--- cli.js
+++ cli.js
@@ -2,10 +2,13 @@
 
 const get = require('lodash/get');
 const key = require('./lib/key');
+const copykey = require('./lib/copykey');
 const generate = require('./lib/generate');
 
 if (get(process.argv, '2') === 'key') {
   key();
+} else if (get(process.argv, '2') === 'copykey') {
+  copykey()
 } else {
   generate();
 }
